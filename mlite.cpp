f3ac460d27a381ceb122c343c9f7499235792639
Jim Northrup <jim@cydesign.com>
2013-12-28 01:25:31 -0800

    down to 2 errors on compile 26 warnings

--- mlite.cpp
+++ mlite.cpp
@@ -163,8 +163,7 @@ struct expected_component : std::exception
 {
     std::string msg;
 
-    expected_component(std::string const& source, std::size_t line
-                     , info const& w)
+    expected_component(std::string const& source, std::size_t line   , info const& w)
     {
         using boost::spirit::basic_info_walker;
 
@@ -360,38 +359,36 @@ namespace sexpr
 template <typename Iterator, typename ErrorHandler = error_handler<Iterator> >
 struct parser : qi::grammar<Iterator, utree(), whitespace<Iterator> >
 {
-    qi::rule<Iterator, utree(), whitespace<Iterator> >
-        start, element, list;
+    qi::rule<Iterator, utree(), whitespace<Iterator> >      start ;
 
-    qi::rule<Iterator, utree()>
-        atom;
 
-    qi::rule<Iterator, int()>
-        integer;
 
-    qi::rule<Iterator, utf8_symbol_type()>
-        symbol;
+    px::function<ErrorHandler> const      error;
 
-    qi::rule<Iterator, utree::nil_type()>
-        nil_;
-
-    qi::rule<Iterator, binary_string_type()>
-        binary;
-
-    utf8::parser<Iterator>
-        string;
-
-    px::function<ErrorHandler> const
-        error;
-
-    tagger<Iterator, save_line_pos>
-        pos;
 
     parser(std::string const& source_file = "<string>"):
         parser::base_type(start), error(ErrorHandler(source_file))
     {
-        using standard::char_;
-        using qi::unused_type;
+        namespace qi = boost::spirit::qi;
+        namespace ascii = boost::spirit::ascii;
+        namespace phoenix = boost::phoenix;
+        using boost::spirit::omit;
+        using boost::spirit::no_skip;
+        using boost::spirit::raw;
+        using boost::spirit::double_;
+        using boost::spirit::utree;
+         using qi::lexeme;
+        using qi::on_error;
+        using qi::fail;
+        using ascii::char_;
+        using ascii::string;
+        using ascii::alpha;
+        using ascii::alnum;
+        using namespace qi::labels;
+        using phoenix::construct;
+        using phoenix::val;
+        using qi::symbols;
+         using qi::unused_type;
         using qi::lexeme;
         using qi::hex;
         using qi::oct;
@@ -410,43 +407,139 @@ struct parser : qi::grammar<Iterator, utree(), whitespace<Iterator> >
         using qi::_3;
         using qi::_4;
 
-        real_parser<double, strict_real_policies<double> > strict_double;
-        uint_parser<unsigned char, 16, 2, 2> hex2;
-        bool_parser<bool, sexpr::bool_input_policies> boolean;
+  qi::rule<Iterator,  utree ,  ascii::space_type> stored_definition;
 
-        start = element.alias();
 
-        element = atom | list;
+        qi::symbols< >  KEYWORDS_TABLE;
+        qi::rule<Iterator,   ascii::space_type> long_class_specifier , short_class_specifier , der_class_specifier , enumeration_literal , external_function_call , import_clause , declaration ,
+           for_index , component_reference , named_argument , primary , language_specification , class_definition , class_prefixes , class_specifier , base_prefix , enum_list , composition ,
+           element , extends_clause , constraining_clause , component_clause , type_prefix , type_specifier , component_list , component_declaration , condition_attribute , modification ,
+           class_modification , argument_list , argument , element_modification_or_replaceable , element_modification , element_redeclaration , element_replaceable , component_clause1 ,
+           component_declaration1 , short_class_definition , equation_section , algorithm_section , element_list ,  equation , statement , if_equation , if_statement ,
+           for_equation , for_statement , for_indices , while_statement , when_equation , when_statement , connect_clause , expression , simple_expression , logical_expression , logical_term ,
+           logical_factor , relation , rel_op , arithmetic_expression , add_op , term , mul_op , factor , bool_op , function_call_args , function_arguments , named_arguments , function_argument ,
+           output_expression_list , expression_list , array_subscripts , subscript , comment , annotation  ,
+           DOT=lit('.'), COLON=lit(':'), DER=lit("der"), ELSE=lit("else"), IF=lit("if"), WHEN=lit("when"), WHILE=lit("while"), THEN=lit("then"), FOR=lit("for"), END=lit("end"), OR=lit("or"),
+           AND=lit("and"), FINAL=lit("final"), COMMA=lit(','), EQ=lit('='), SEMICOLON=lit(','), LOOP=lit("loop"), ELSEWHEN=lit("elsewhen"), ELSEIF=lit("elseif"), FUNCTION=lit("function"),
+           CLASS=lit("class"),
+           INITIAL=lit("initial");
 
-        list = pos(_val, '(') > *element > ')';
+         qi::rule<Iterator, std::string(), ascii::space_type> IDENT,name,string_comment,
+                      NONDIGIT            ,// char_("_") |ascii::alpha,
+           DIGIT               ,// ascii::digit,
+           KEYWORDS            ,// lexeme[KEYWORDS_TABLE >> !(NONDIGIT|DIGIT)],
+           UNSIGNED_INTEGER    ,// lexeme[+ascii::digit],
+           UNSIGNED_NUMBER     ,// UNSIGNED_INTEGER >> -(char_(".") >> -UNSIGNED_INTEGER ) >> -((char_("e")|char_("E")) >> -(char_("+") | char_("-")) >> UNSIGNED_INTEGER),
+           S_CHAR              ,// char_ - (char_("\\")|char_("\"")),
+           Q_CHAR              ,// char_ - (char_("\\")|char_("'")),
+           S_ESCAPE            ,// (lit("\\'") | lit("\\\"") | lit("\\?") | lit("\\\\") |  lit("\\a") | lit("\\b")  | lit("\\f") | lit("\\n")  | lit("\\r") | lit("\\t") | lit("\\v")),
+           STRING              ,// char_("\"") >> *(S_CHAR | S_ESCAPE) >> char_("\""),
+           Q_IDENT             ;// char_("'") >> (Q_CHAR | S_ESCAPE) >> *(Q_CHAR | S_ESCAPE) >> char_("'") ;
 
-        atom = nil_
-             | strict_double
-             | integer
-             | boolean
-             | string
-             | symbol
-             | binary;
 
-        nil_ = qi::attr_cast(lit("nil"));
+        KEYWORDS_TABLE ="algorithm", "and", "annotation", "assert", "block", "break", "class", "connect", "connector", "constant", "constrainedby", "der", "discrete", "each", "else", "elseif",
+        "elsewhen", "encapsulated", "end", "enumeration", "equation", "expandable", "extends", "external", "false", "final", "flow", "for", "function", "import", "if", "in", "initial", "inner",
+        "input", "loop", "model", "not", "or", "outer", "output", "package", "parameter", "partial", "protected", "public", "record", "redeclare", "replaceable", "return", "then", "true",
+        "type", "when", "while", "within";
 
-        integer = lexeme[ no_case["#x"] >  hex]
-                | lexeme[ no_case["#o"] >> oct]
-                | lexeme[-no_case["#d"] >> int_];
 
-        std::string exclude = std::string(" ();\"\x01-\x1f\x7f") + '\0';
-        symbol = lexeme[+(~char_(exclude))];
+           NONDIGIT            %= char_("_") |ascii::alpha;
+           DIGIT               %= ascii::digit;
+           KEYWORDS            %= lexeme[KEYWORDS_TABLE >> !(NONDIGIT|DIGIT)];
+           UNSIGNED_INTEGER    %= lexeme[+ascii::digit];
+           UNSIGNED_NUMBER     %= UNSIGNED_INTEGER >> -(char_(".") >> -UNSIGNED_INTEGER ) >> -((char_("e")|char_("E")) >> -(char_("+") | char_("-")) >> UNSIGNED_INTEGER);
+           S_CHAR              %= char_ - (char_("\\")|char_("\""));
+           Q_CHAR              %= char_ - (char_("\\")|char_("'"));
+           S_ESCAPE            %= (lit("\\'") | lit("\\\"") | lit("\\?") | lit("\\\\") |  lit("\\a") | lit("\\b")  | lit("\\f") | lit("\\n")  | lit("\\r") | lit("\\t") | lit("\\v"));
+           STRING              %= lit('"') >> *(S_CHAR | S_ESCAPE) >> '"';
+           Q_IDENT             %= lit('\'') >> +(Q_CHAR | S_ESCAPE)  >> '\'' ;
+           name  %= -DOT >> IDENT %DOT ;
+           IDENT  %= raw[(lexeme[NONDIGIT >> *(NONDIGIT | DIGIT)] | Q_IDENT) - KEYWORDS],name = IDENT >> -(char_('.') >> IDENT);
 
-        binary = lexeme['#' > *hex2 > '#'];
+        class_definition = -lit("encapsulated" ) >> class_prefixes >> class_specifier ;
+        class_specifier = long_class_specifier | short_class_specifier | der_class_specifier ;
+        class_prefixes =   -lit("partial")  >> ( CLASS | "model" | "record" | "block"| -lit("expandable") >> "connector" | "type" | "package" | FUNCTION | "operator" >> -( FUNCTION | "record" ) ) ;
+        long_class_specifier = IDENT >> string_comment >> composition >> END >> IDENT | "extends">> IDENT >> -class_modification >> string_comment >> composition >> END >> IDENT ;
+        short_class_specifier = IDENT >> EQ >> base_prefix >> name >> -array_subscripts >> -class_modification >> comment | IDENT >> EQ >> "enumeration" >> '(' >> ( -enum_list | ':' ) >> ')' >> comment ;
+        der_class_specifier = IDENT >> EQ >> DER >> '(' >> name >> +( COMMA >> IDENT ) >> ')' >> comment ;
+        enumeration_literal = IDENT >> comment ;
+        external_function_call = -( component_reference >> EQ ) >> IDENT >> '(' >> -expression_list >> ')' ;
+        import_clause = lit("import" ) >> ( IDENT >> EQ >> name | name >> -( DOT >> '*' | ".*" ) ) >> comment ;
+        declaration = IDENT >> -array_subscripts >> -modification ;
+        for_index = IDENT >> -( lit("in") >> expression ) ;
 
-        start.name("sexpr");
-        element.name("element");
-        list.name("list");
-        atom.name("atom");
-        nil_.name("nil");
-        integer.name("integer");
-        symbol.name("symbol");
-        binary.name("binary");
+        component_reference = -DOT >> IDENT >> -array_subscripts >> *( DOT >> IDENT >> -array_subscripts ) ;
+        named_argument = IDENT >> EQ >> function_argument ;
+        primary = UNSIGNED_NUMBER | STRING | bool_op | component_reference | ( name | DER| INITIAL) >> function_call_args | '(' >> output_expression_list >> ')' | '[' >> expression_list%';'  >> ']' | '{' >> function_arguments >> '}' | END;
+        string_comment = -( STRING%'+'  ) ;
+        language_specification = STRING ;
+        base_prefix = type_prefix ;
+        enum_list = enumeration_literal % COMMA  ;
+        composition = element_list >> *( lit("public") >> element_list | "protected" >> element_list | equation_section | algorithm_section ) >> -( lit("external") >> -language_specification >> -external_function_call >> -annotation >> SEMICOLON ) >> -( annotation >> SEMICOLON ) ;
+        element = import_clause | extends_clause |  -(lit("redeclare") ) >> -FINAL >> -lit("inner") >> -( lit("outer") ) >> ( class_definition | component_clause | lit("replaceable" ) >> ( class_definition | component_clause ) >> -( constraining_clause >> comment ) ) ;
+        extends_clause = lit("extends") >> name >> -class_modification >> -annotation ;
+        constraining_clause = lit("constrainedby") >> name >> -class_modification ;
+        component_clause = type_prefix >> type_specifier >> -array_subscripts >> component_list ;
+        type_prefix = -( lit("flow") | "stream") >> -( lit("discrete") | "parameter" | "constant") >> -( lit("input") | "output") ;
+        type_specifier = name ;
+        component_list = component_declaration %COMMA  ;
+        component_declaration = declaration >> -condition_attribute >> comment ;
+        condition_attribute = IF >> expression ;
+        modification = class_modification >> -( (EQ |":=" ) >> expression );
+        class_modification = lit('(') >> -argument_list >> ')' ;
+        argument_list = argument %COMMA  ;
+        argument = element_modification_or_replaceable | element_redeclaration ;
+        element_modification_or_replaceable = -lit("each") >> -FINAL  >> ( element_modification | element_replaceable ) ;
+        element_modification = name >> -modification >> string_comment ;
+        element_redeclaration = lit("redeclare") >> -lit("each") >> -FINAL  >> ( short_class_definition | component_clause1 | element_replaceable ) ;
+        element_replaceable = lit("replaceable" ) >> ( short_class_definition | component_clause1 ) >> -constraining_clause ;
+        component_clause1 = type_prefix >> type_specifier >> component_declaration1 ;
+        component_declaration1 = declaration >> comment ;
+        short_class_definition = class_prefixes >> short_class_specifier ;
+
+        equation_section = -INITIAL >> "equation" >> *( equation >> SEMICOLON ) ;
+        algorithm_section = -INITIAL >> "algorithm" >> *( statement >> SEMICOLON ) ;
+        element_list = *( element >> SEMICOLON ) ;
+        equation = ( simple_expression >> EQ >> expression | if_equation | for_equation | connect_clause | when_equation | name >> function_call_args ) >> comment ;
+        statement = ( component_reference >> (lit( ":=" ) >> expression | function_call_args ) | '(' >> output_expression_list >> ')' >> ":=" >> component_reference >> function_call_args | "break" | "return"| if_statement | for_statement | while_statement | when_statement ) >> comment ;
+        if_equation = IF >> expression >> THEN >> *( equation >> SEMICOLON ) >> *( ELSEIF >> expression >> THEN >> *( equation >> SEMICOLON ) ) >> -( ELSE >> *( equation >> SEMICOLON ) ) >> END >> IF ;
+        if_statement = IF >> expression >> THEN >> *( statement >> SEMICOLON ) >> *( ELSEIF >> expression >> THEN >> *( statement >> SEMICOLON ) ) >> -( ELSE >> *( statement >> SEMICOLON ) ) >> END >> IF ;
+        for_equation = FOR >> for_indices >> LOOP >> *( equation >> SEMICOLON ) >> END >> FOR ;
+        for_statement = FOR >> for_indices >> LOOP >> *( statement >> SEMICOLON ) >> END >> FOR ;
+        for_indices = for_index %COMMA   ;
+        while_statement = WHILE >> expression >> LOOP >> *( statement >> SEMICOLON ) >> END >> WHILE ;
+        when_equation = WHEN >> expression >> THEN >> *( equation >> SEMICOLON ) >> *( ELSEWHEN >> expression >> THEN >> *( equation >> SEMICOLON ) ) >> END >> WHEN ;
+        when_statement = WHEN >> expression >> THEN >> *( statement >> SEMICOLON ) >> *( ELSEWHEN >> expression >> THEN >> *( statement >> SEMICOLON ) ) >> END >> WHEN ;
+        connect_clause = lit("connect") >> '(' >> component_reference >> COMMA >> component_reference >> ')' ;
+        expression = simple_expression | IF >> expression >> THEN >> expression >> *( ELSEIF >> expression >> THEN >> expression ) >> ELSE >> expression ;
+        simple_expression = logical_expression >> -( COLON >> logical_expression >> -( COLON >> logical_expression ) ) ;
+        logical_expression = logical_term % OR   ;
+        logical_term = logical_factor% AND   ;
+        logical_factor = -lit("not") >> relation ;
+        relation = arithmetic_expression >> -( rel_op >> arithmetic_expression ) ;
+        rel_op = lit('<') | "<=" | '>' | ">=" | "==" | "<>" ;
+        arithmetic_expression = -add_op >> term % add_op  ;
+        add_op = lit('+') | '-' | ".+" | ".-" ;
+        term = factor % mul_op ;
+        mul_op = lit('*') | '/' | ".*" | "./" ;
+        factor = primary >> -( ( lit('^') | ".^" ) >> primary ) ;
+        bool_op = lit("false") | "true" ;
+        function_call_args = lit('(') >> -function_arguments >> ')' ;
+        function_arguments = function_argument >> -( COMMA >> function_arguments | FOR >> for_indices ) | named_arguments ;
+        named_arguments = named_argument % COMMA ;
+        function_argument = FUNCTION >> name >> '(' >> -named_arguments >> ')' | expression ;
+        output_expression_list = -expression >> *( COMMA >> -expression ) ;
+        expression_list = expression %COMMA;
+        array_subscripts = lit('[') >> subscript % COMMA >> ']' ;
+        subscript = COLON | expression ;
+        comment = string_comment >> -annotation ;
+        annotation = lit("annotation") >> class_modification;
+
+        stored_definition= -( lit("within") >> -name >> SEMICOLON ) >> *( -FINAL >> class_definition >> SEMICOLON ) ;
+
+
+        start =  stored_definition;
+        start.name("start");
 
         on_error<fail>(start, error(_1, _2, _3, _4));
     }
@@ -528,12 +621,7 @@ namespace phoenix = boost::phoenix;
         using phoenix::construct;
         using phoenix::val;
         using qi::symbols;
-        namespace sexpr
-{
 
-template <typename Iterator, typename ErrorHandler = error_handler<Iterator> >
-struct parser : qi::grammar<Iterator, utree(), whitespace<Iterator> >
-{
 
         qi::symbols< >  KEYWORDS_TABLE;
         qi::rule<Iterator,   ascii::space_type> long_class_specifier , short_class_specifier , der_class_specifier , enumeration_literal , external_function_call , import_clause , declaration ,
